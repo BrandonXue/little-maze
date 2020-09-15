e3d9068a439d70c8c330a0ce796cf0f982e42f2c
BrandonXue <brandon_xue@hotmail.com>
2020-09-14 22:22:46 -0700

    Kruskal's initial commit

--- maze.js
+++ maze.js
@@ -346,3 +346,41 @@ function binary_space_partition(left_col, right_col, top_row, bot_row, dir, maze
         binary_space_partition(wall_choice+1, right_col, top_row, bot_row, wall_dir, maze);
     }
 }
+
+class DisjointSet {
+    constructor(starting_sets) {
+        if (starting_sets < 256) { // 2^8
+            this.sets = new Uint8Array(starting_sets);
+        } else if (starting_sets < 65536) { // 2^16
+            this.sets = new Uint16Array(starting_sets);
+        } else if (starting_sets < 4294967296) { // 2^32
+            this.sets = new Uint32Array(starting_sets);
+        } else {
+            this.sets = new Array(starting_sets);
+        }
+        this.num_sets = starting_sets;
+    }
+
+    get_update_root(set_index) {
+        if (this.sets[set_index] == 0) // The current node is the root of its tree
+            return set_index;
+
+        // Else, sets[set_index] will hold the index of its parent
+        // Recursively find the root, updating each node in the path
+        let root = this.get_update_root(this.sets[set_index]);
+        this.sets[set_index] = root;
+        return root; // Finally return root
+    }
+
+    try_join_trees(index1, index2) {
+        let root1 = this.get_update_root(index1);
+        let root2 = this.get_update_root(index2);
+        // If both nodes are already part of the same tree, return false
+        if (root1 == root2)
+            return false;
+        // Else, make tree 2's root point to tree 1's root
+        this.sets[root2] = root1;
+        this.num_sets -= 1; // Decrement total number of sets
+        return true;
+    }
+}
